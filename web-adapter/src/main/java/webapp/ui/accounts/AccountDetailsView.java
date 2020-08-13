9d96c8b47477600bcd0942cd8dc2e3f7e49114a7
m_afattah <ripple_92@live.com>
2020-08-13 15:30:14 +0200

    Fix account details view

--- web-adapter/src/main/java/webapp/ui/accounts/AccountDetailsView.java
+++ web-adapter/src/main/java/webapp/ui/accounts/AccountDetailsView.java
@@ -2,7 +2,8 @@ package webapp.ui.accounts;
 
 import com.vaadin.flow.component.Text;
 import com.vaadin.flow.component.grid.Grid;
-import com.vaadin.flow.component.html.H2;
+import com.vaadin.flow.component.html.H3;
+import com.vaadin.flow.component.html.H4;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.data.provider.DataProvider;
@@ -23,13 +24,13 @@ public class AccountDetailsView extends VerticalLayout implements HasUrlParamete
 
     private final LookupAccounts accounts;
 
-    private final H2 id;
+    private final H3 id;
     private final Text balance;
     private final Grid<ActivityModel> activities;
 
     private AccountDetailsView(LookupAccounts accounts) {
         this.accounts = accounts;
-        this.id = new H2();
+        this.id = new H3();
         this.balance = new Text("");
         this.activities = new Grid<>();
         this.activities.addColumn(model -> model.source).setHeader("Source Account");
@@ -37,8 +38,13 @@ public class AccountDetailsView extends VerticalLayout implements HasUrlParamete
         this.activities.addColumn(model -> model.money).setHeader("Amount");
         this.activities.addColumn(model -> model.timestamp).setHeader("Time");
         final HorizontalLayout layout = new HorizontalLayout();
+        layout.setAlignItems(Alignment.CENTER);
         layout.addAndExpand(this.id);
-        layout.add(this.balance);
+        final VerticalLayout another = new VerticalLayout();
+        another.setAlignItems(Alignment.CENTER);
+        another.add(new H4("Balance"), this.balance);
+        another.setWidth("fit-content");
+        layout.add(another);
         this.add(layout, this.activities);
     }
 
